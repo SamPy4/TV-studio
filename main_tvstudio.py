6e92fa3490828355f39a0acc9d108d9084b84a17
SamPy4 <sami.porio@gmail.com>
2017-08-24 15:10:42 +0300

    modifed

---
+++ main_tvstudio.py
@@ -0,0 +1,16 @@
+import tkinter as tk
+from tavara import *
+
+hylly1 = hylly()
+
+tavara1 = tavara("DMX", "piuha", "hylly1", 5)
+
+hylly1.lisaaTavara(tavara1)
+
+
+line0 = hylly1.getTavara(0)[0]
+
+print(line0)
+lista = line0.split(";")
+
+print(lista)
